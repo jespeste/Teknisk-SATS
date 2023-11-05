3511f89fd9bc8117cf8dcd64a3db474e688de122
jespeste <jespeste@stud.ntnu.no>
2023-11-05 21:49:12 +0100

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello!</h1>
+        <p>What is going on ere then?</p>
+        <ul>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+          <li>I am good</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
